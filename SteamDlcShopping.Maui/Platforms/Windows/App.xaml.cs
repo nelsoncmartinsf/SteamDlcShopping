3156141efc568880a6b4472d6f47950f9ea1aa4c
Nélson Martins <nelson.martins@winrestbooking.com>
2023-06-14 15:57:24 +0100

    Attempt to fix window size.

--- SteamDlcShopping.Maui/Platforms/Windows/App.xaml.cs
+++ SteamDlcShopping.Maui/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -17,6 +22,21 @@ namespace SteamDlcShopping.Maui.WinUI
         public App()
         {
             this.InitializeComponent();
+
+            // Nothing like a bit of magic to make the window size fixed... What a nice start...
+            //WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, _) =>
+            //{
+            //    handler.PlatformView.GetAppWindow().Resize(new SizeInt32(1280, 720));
+
+            //    IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(handler.PlatformView);
+            //    WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+            //    AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+            //    OverlappedPresenter presenter = appWindow.Presenter as OverlappedPresenter;
+            //    presenter.IsResizable = false;
+            //    presenter.IsMaximizable = false; // It can still maximize...
+            //});
+            // Actually, screw that, let the user mess with the size.
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
