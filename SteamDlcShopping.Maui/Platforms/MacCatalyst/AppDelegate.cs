cd6787bd85bcacb0d944664b119d9f882b5bdb28
Nélson Martins <nelson.martins@winrestbooking.com>
2023-06-14 14:43:13 +0100

    MAUI project creation.

---
+++ SteamDlcShopping.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SteamDlcShopping.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
