d29f0aac400e82aee5fe01e5ba126f5011d307e4
Nélson Martins <nelson.martins@winrestbooking.com>
2023-06-20 15:30:30 +0100

    Initial work on MainPage model

---
+++ SteamDlcShopping.Maui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SteamDlcShopping.Maui.ViewModels;
+internal class MainPageViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public ICommand SignInCommand { get; init; }
+    public ICommand SignOutCommand { get; init; }
+
+    public ICommand SettingsCommand { get; init; }
+    public ICommand AboutCommand { get; init; }
+
+
+}
